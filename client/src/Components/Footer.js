f4f9e7de40221ce855dc4bff3143c9b0af166636
aya222-utas <blackmarshmallow800@gmail.com>
2025-12-07 00:27:21 +0400

    second commit

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>--Write your name--|©2024|PostIT|All Rights Reserved.</div>
+      <div> Developed by Aya Said |©2024|PostIT|All Rights Reserved.</div>
     </footer>
   );
 };
